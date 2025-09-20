36f508e08ecdca654bfb9675de5bc8c667f58371
tygronaut <141247889+tygronaut@users.noreply.github.com>
2025-09-20 21:47:49 +0000

    First test on website

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.tygron.com/nl');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle("Tygron Platform – Accelerated Simulations for Engineers");
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.tygron.com/nl');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('button', { name: 'Free Trial' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByRole('paragraph', { name: 'Start uw 10 dagen free trial' })).toBeVisible();
 });
